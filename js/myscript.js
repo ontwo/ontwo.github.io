3d0919fc97347ef031c371b5d5cc5ba4e91a182b
ontwo <mainonetwo@gmail.com>
2015-04-13 17:22:45 +0300

    old files deleted

--- js/myscript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".account-menu-enter").click(function(){
-        $(".popup-login").css("display", "block");
-    });
-    $(".btn-close").click(function(){
-        $(".popup-login").css("display", "none");
-    });
-});
